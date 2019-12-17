6edaceef9036de54bbfa4c68f3efb658a4b0feb6
MhmdAbed11 <mohammad_aabed@hotmail.com>
2019-12-17 23:33:31 +0200

    add apollo client

--- pages/register/register.tsx
+++ pages/register/register.tsx
@@ -8,7 +8,63 @@ import {
   faLock
 } from "@fortawesome/free-solid-svg-icons";
 import PrimaryButton from "../../components/shared/PrimaryButton/PrimaryButton";
+import gql from "graphql-tag";
+import { useLazyQuery, useMutation } from "@apollo/react-hooks";
+
+export const SIGNUP_MUTATION = gql`
+  mutation UserMutation {
+    signup(
+      password: "admin"
+      firstName: "Mohammad"
+      confirmPassword: "admin"
+      lastName: "Abed"
+      email: "mohammad_aaabed@hotmail.com"
+      country: "Lebanon"
+    ) {
+      token
+      user {
+        id
+        firstName
+        lastName
+        country
+        email
+      }
+    }
+  }
+`;
+const GET_USER_QUERY = gql`
+  {
+    user(id: "5df9491024aa9a00076692ac") {
+      id
+      firstName
+      lastName
+      email
+      country
+      createdAt
+      updatedAt
+      posts {
+        id
+        title
+      }
+    }
+  }
+`;
+
 export default function Register() {
+  const [signUp, { data }] = useMutation(SIGNUP_MUTATION);
+  const [getData, userData] = useLazyQuery(GET_USER_QUERY);
+  if (!userData) {
+    getData();
+  }
+  if (userData) {
+    console.log(userData);
+  }
+
+  // if (data) {
+  //   // const { token } = data.signup;
+  //   // localStorage.setItem("token", token);
+  //   // console.log(data);
+  // }
   return (
     <section className="register-section">
       <div className="container">
@@ -65,7 +121,15 @@ export default function Register() {
                     />
                   </div>
                 </div>
-                <PrimaryButton fullWidth>Register</PrimaryButton>
+                <PrimaryButton
+                  fullWidth
+                  onClick={e => {
+                    e.preventDefault();
+                    signUp();
+                  }}
+                >
+                  Register
+                </PrimaryButton>
               </form>
             </div>
           </div>
